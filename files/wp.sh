97c50f0ef79d3807d1ba4882e4d8103ba491772f
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-06-25 00:38:58 +0700

    New Playbook Added

--- files/wp.sh
+++ files/wp.sh
@@ -6,12 +6,12 @@ sudo chkconfig httpd on
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
 aws s3 cp s3://scbdemo/wordpress/ wordpress/ --recursive
 sudo cp -r wordpress/* /var/www/html/
 sudo chown -R apache /var/www
 sudo chgrp -R apache /var/www
 sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type d -exec sudo chmod 2775 {} \;
 sudo service httpd restart
